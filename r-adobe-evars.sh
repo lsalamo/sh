99032116b5f3b33166535281c50b69b053781bf6
Luis José Salamó Esteve <lsalamo@gmail.com>
2021-11-30 12:24:24 +0100

    test

--- r-adobe-evars.sh
+++ r-adobe-evars.sh
@@ -13,7 +13,7 @@ fi
 # ACTIVAMOS ENTORNO R
 source ~/Applications/anaconda3/etc/profile.d/conda.sh
 conda activate r_env
-echo "INFO > conda environment 'r_env' activated"ddd
+echo "INFO > conda environment 'r_env' activated"
 
 # RUN SCRIPT R
 echo "INFO > RScript init"
